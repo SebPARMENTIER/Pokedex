12fa31eccbdbb1167a788d406254119130887288
SebPARMENTIER <sebastien.parmentier@outlook.fr>
2021-11-23 15:08:24 +0100

    Add pool in database.js

--- app/database.js
+++ app/database.js
@@ -4,7 +4,19 @@ const sequelize = new Sequelize(process.env.DATABASE_URL,{
     define: {
       timestamps: false
     },
-    logging: false
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000,
+      acquire: 30000
+    },
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
 });
 
 module.exports = sequelize;
